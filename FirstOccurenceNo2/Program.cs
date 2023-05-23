4ca0ca276d51c9d383b66cf81b68d03081ebd64e
Melusi Malebo <thandohmm@gmail.com>
2023-05-23 13:36:00 +0200

    Add project files.

---
+++ FirstOccurenceNo2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstOccurenceNo2
+{
+    using System;
+
+    namespace FirstOccurrenceWord
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                string sentence = "THE QUICK BROWN FOX JUMPS OVER A LAZY DOG.";
+
+                Console.WriteLine("Enter a letter:");
+                char letter = Console.ReadLine()[0];
+                char uppercaseLetter = char.ToUpper(letter);
+
+                int position = sentence.IndexOf(uppercaseLetter);
+                int nextSpacePosition = sentence.IndexOf(' ', position);
+
+                string word = sentence.Substring(position + 1, nextSpacePosition - position - 1);
+
+                Console.WriteLine($"{uppercaseLetter} first occurrence in word {word}");
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
